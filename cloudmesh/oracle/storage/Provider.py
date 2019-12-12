f736be363162dcc2690facb990e012bace67dc81
katukota <shivani.katukota@gmail.com>
2019-12-12 10:13:55 -0500

    oracle storage functions implemented

--- cloudmesh/oracle/storage/Provider.py
+++ cloudmesh/oracle/storage/Provider.py
@@ -1,6 +1,7 @@
 import oci
 from pprint import pprint
 import os
+from pathlib import Path
 
 from cloudmesh.storage.StorageNewABC import StorageABC
 from cloudmesh.configuration.Config import Config
@@ -34,17 +35,18 @@ class Provider(StorageABC):
         :param config: TBD
         """
         super().__init__(service=service, config=config)
+
+        # Get credentials
         configure = Config(config)["cloudmesh"]["storage"]["oracle"][
             "credentials"]
         credential = self._get_credentials(configure)
-        virtual_network = oci.core.VirtualNetworkClient(credential)
-        identity_client = oci.identity.IdentityClient(credential)
         self.object_storage = oci.object_storage.ObjectStorageClient(credential)
         self.compartment_id = credential["compartment_id"]
-
         self.namespace = self.object_storage.get_namespace().data
-        self.bucket_name = "example-bucket"
-        object_name = "example-object"
+
+        # Get defaults
+        self.bucket_name = Config(config)["cloudmesh"]["storage"]["oracle"][
+            "default"]["bucket"]
         self.storage_dict = {}
 
     def update_dict(self, elements, kind=None):
@@ -60,38 +62,52 @@ class Provider(StorageABC):
             d.append(entry)
         return d
 
+    def ls_files(self, dir_path, recursive):
+        files = []
+        for item in os.listdir(dir_path):
+            abspath = os.path.join(dir_path, item)
+            try:
+                if os.path.isdir(abspath):
+                    if recursive:
+                        files = files + self.ls_files(abspath)
+                else:
+                    files.append(abspath)
+            except FileNotFoundError:
+                print('Invalid Directory')
+        return files
+
     # function to massage file path and do some transformations
     # for different scenarios of file inputs
     @staticmethod
-    def massage_path(file_name_path):
-        massaged_path = file_name_path
+    def get_os_path(file_name_path):
+        os_path = Path(file_name_path)
+        return os_path
 
-        # convert possible windows style path to unix path
-        massaged_path = massaged_path.replace('\\', '/')
-
-        # remove leading slash symbol in path
-        if len(massaged_path) > 0 and massaged_path[0] == '/':
-            massaged_path = massaged_path[1:]
-
-        # expand home directory in path
-        massaged_path = massaged_path.replace('~', os.path.expanduser('~'))
-        # pprint(massaged_path)
-
-        # expand possible current directory reference in path
-        if massaged_path[0:2] == '.\\' or massaged_path[0:2] == './':
-            massaged_path = os.path.abspath(massaged_path)
-
-        return massaged_path
-
-    # Function to extract obj dict from metadata
     @staticmethod
     def extract_file_dict(filename, metadata):
+
         info = {
             "fileName": filename,
             "lastModificationDate":
                 metadata['last-modified'],
             "contentLength":
                 metadata['Content-Length']
+        }
+        return info
+
+    # Function to extract obj dict from metadata
+    def get_and_extract_file_dict(self, filename):
+
+        metadata = self.object_storage.head_object(
+            self.namespace, self.bucket_name,
+            filename)
+
+        info = {
+            "fileName": filename,
+            "lastModificationDate":
+                metadata.headers['last-modified'],
+            "contentLength":
+                metadata.headers['Content-Length']
         }
         return info
 
@@ -106,13 +122,21 @@ class Provider(StorageABC):
         bucket = self.object_storage.create_bucket(self.namespace, request)
         print("Bucket Created:", name)
 
+        # Update in db
         self.storage_dict['action'] = 'bucket_create'
         self.storage_dict['bucket'] = name
         self.bucket_name = name
         self.storage_dict['message'] = 'Bucket created'
-        self.storage_dict['objlist'] = [self.extract_file_dict(name, bucket.headers)]
-
+        self.storage_dict['objlist'] = []
+        self.storage_dict['objlist'].append({
+            "fileName": name,
+            "lastModificationDate":
+                bucket.headers['Date'],
+            "contentLength":
+                bucket.headers['Content-Length']
+        })
         dictObj = self.update_dict(self.storage_dict['objlist'])
+
         return dictObj
 
     def bucket_exists(self, name=None):
@@ -135,7 +159,7 @@ class Provider(StorageABC):
         print("Creating directories without creating a file is not supported "
               "in Oracle")
 
-    def list(self, source=None, dir_only=False, recursive=False):
+    def list(self, source=None, dir_only=False, recursive=True):
         """
         lists the information as dict
 
@@ -146,34 +170,34 @@ class Provider(StorageABC):
         :return: dict
 
         """
-        if source is None:
-            source = self.bucket_name
 
         self.storage_dict['action'] = 'list'
         self.storage_dict['source'] = source
         self.storage_dict['recursive'] = recursive
-
-        objs = self.object_storage.list_objects(self.namespace,
-                                                source).data.objects
         dir_files_list = []
-        for obj in objs:
-            metadata = self.object_storage.get_object(self.namespace, source,
-                                                      obj.name)
-            dir_files_list.append(self.extract_file_dict(obj.name,
-                                                         metadata.headers))
 
-        if len(dir_files_list) == 0:
-            print("No files found in directory")
-            self.storage_dict['message'] = ''
+        if source is None:
+            # Get all items from bucket
+            objs = self.object_storage.list_objects(
+                self.namespace, self.bucket_name).data.objects
         else:
-            self.storage_dict['message'] = dir_files_list
+            # Get items from bucket that start with name 'source'
+            source = self.get_os_path(source)
+
+            objs = self.object_storage.list_objects(
+                self.namespace, self.bucket_name, prefix=str(
+                    source)).data.objects
+
+        # Extract information of matched objects
+        for obj in objs:
+            dir_files_list.append(self.get_and_extract_file_dict(obj.name))
 
         self.storage_dict['objlist'] = dir_files_list
         dictObj = self.update_dict(self.storage_dict['objlist'])
         return dictObj
 
     # function to delete file or directory
-    def delete(self, source=None, recursive=False):
+    def delete(self, source=None, recursive=True):
         """
         deletes the source
         :param source: the source which either can be a directory or file
@@ -181,28 +205,32 @@ class Provider(StorageABC):
                           subdirectories in the specified source
         :return: dict
         """
+
+        if source is None:
+            print("Please enter the file/directory name to delete.")
+            exit(0)
         self.storage_dict['action'] = 'delete'
         self.storage_dict['source'] = source
         self.storage_dict['recursive'] = recursive
-        trimmed_source = self.massage_path(source)
-        dict_obj = None
+        trimmed_source = self.get_os_path(source)
+        dict_obj = []
 
-        try:
-            file_data = self.object_storage.get_object(self.namespace,
-                                                       self.bucket_name,
-                                                       trimmed_source)
+        objs = self.object_storage.list_objects(
+            self.namespace, self.bucket_name, prefix=str(trimmed_source))
+
+        print(objs.data.objects)
+        for obj in objs.data.objects:
+            # Save deleted object details to be updated in the db
+            dict_obj.append(self.get_and_extract_file_dict(obj.name))
+
+            # Delete object
             self.object_storage.delete_object(self.namespace,
-                                                         self.bucket_name,
-                                                         trimmed_source)
+                                              self.bucket_name,
+                                              obj.name)
 
-            self.storage_dict['message'] = 'Source Deleted'
-            self.storage_dict['objlist'] = [self.extract_file_dict(
-                trimmed_source, file_data.headers)]
-            dict_obj = self.update_dict(self.storage_dict['objlist'])
-        except:
-            print("File not found")
-            
-        return dict_obj
+        self.storage_dict['message'] = 'Source Deleted'
+        self.storage_dict['objlist'] = dict_obj
+        return self.update_dict(self.storage_dict['objlist'])
 
     # function to upload file
     def put(self, source=None, destination=None, recursive=False):
@@ -214,45 +242,57 @@ class Provider(StorageABC):
                           subdirectories in the specified source
         :return: dict
         """
-        # check if the source and destination roots exist
+
         self.storage_dict['action'] = 'put'
         self.storage_dict['source'] = source
         self.storage_dict['destination'] = destination
         self.storage_dict['recursive'] = recursive
         pprint(self.storage_dict)
 
-        trimmed_source = self.massage_path(source)
-        trimmed_destination = self.massage_path(destination)
+        trimmed_source = self.get_os_path(source)
+        trimmed_destination = self.get_os_path(destination)
         is_source_file = os.path.isfile(trimmed_source)
-
+        is_source_dir = os.path.isdir(trimmed_source)
         files_uploaded = []
 
-        bucket = self.bucket_name
-        if not self.bucket_exists(bucket):
-            self.bucket_create(bucket)
+        if not self.bucket_exists(self.bucket_name):
+            self.bucket_create(self.bucket_name)
 
         if is_source_file is True:
             # Its a file and need to be uploaded to the destination
-            # check if trimmed_destination is file or a directory
-
             self.object_storage.put_object(self.namespace,
                                            self.bucket_name,
-                                           trimmed_destination,
+                                           str(trimmed_destination),
                                            open(trimmed_source, 'r'))
 
             # make head call since file upload does not return
             # obj dict to extract meta data
-            metadata = self.object_storage.get_object(self.namespace,
-                                                      self.bucket_name,
-                                                      trimmed_destination)
             files_uploaded.append(
-                self.extract_file_dict(trimmed_source, metadata.headers))
+                self.get_and_extract_file_dict(str(trimmed_destination)))
 
             self.storage_dict['message'] = 'Source uploaded'
-            self.storage_dict['objlist'] = files_uploaded
-            dictObj = self.update_dict(self.storage_dict['objlist'])
-            return dictObj
-        return None
+        elif is_source_dir is True:
+            # Its a directory, get all files from the directory to upload
+            for f in self.ls_files(trimmed_source, recursive):
+
+                # Object upload
+                self.object_storage.put_object(
+                    self.namespace, self.bucket_name,
+                    str(trimmed_destination / f), open(str(trimmed_source / f),
+                                                       'rb'))
+
+                # Extract header data from object
+                files_uploaded.append(
+                    self.get_and_extract_file_dict(str(
+                        trimmed_destination / f)))
+
+            self.storage_dict['message'] = 'Source uploaded'
+        else:
+            self.storage_dict['message'] = 'Source not found'
+
+        self.storage_dict['objlist'] = files_uploaded
+        pprint(self.storage_dict)
+        return self.update_dict(self.storage_dict['objlist'])
 
     # function to download file or directory
     def get(self, source=None, destination=None, recursive=False):
@@ -269,32 +309,42 @@ class Provider(StorageABC):
         self.storage_dict['destination'] = destination
         self.storage_dict['recursive'] = recursive
 
-        trimmed_source = self.massage_path(source)
-        trimmed_destination = self.massage_path(destination)
+        trimmed_source = self.get_os_path(source)
+        trimmed_destination = self.get_os_path(destination)
 
-        file_obj = self.object_storage.get_object(self.namespace,
-                                                  self.bucket_name,
-                                                  trimmed_source)
+        file_objs = self.object_storage.list_objects(
+            self.namespace, self.bucket_name, prefix=str(trimmed_source))
 
         files_downloaded = []
         is_target_dir = os.path.isdir(trimmed_destination)
 
-        if file_obj.data:
-            try:
-                if is_target_dir:
-                    f = open(trimmed_destination+trimmed_source, "w+")
-                    f.write(file_obj.data)
-                    f.close()
-                else:
-                    f = open(trimmed_destination, "w+")
-                    f.write(file_obj.data)
-                    f.close()
+        if len(file_objs.data.objects) > 1 and not is_target_dir:
+            print("Please provide a directory to copy multiple files.")
+        else:
+            for file_obj in file_objs.data.objects:
 
-                files_downloaded.append(
-                    self.extract_file_dict(trimmed_source, file_obj.headers))
-                self.storage_dict['message'] = 'Source downloaded'
-            except FileNotFoundError as e:
-                self.storage_dict['message'] = 'Destination not found'
+                obj_data = self.object_storage.get_object(self.namespace,
+                                                          self.bucket_name,
+                                                          file_obj.name)
+
+                try:
+                    if is_target_dir:
+                        with open(trimmed_destination / trimmed_source, 'wb') as f:
+                            for chunk in obj_data.data.raw.stream(
+                                    1024 * 1024, decode_content=False):
+                                f.write(chunk)
+                    else:
+                        with open(trimmed_destination, 'wb') as f:
+                            for chunk in obj_data.data.raw.stream(
+                                    1024 * 1024, decode_content=False):
+                                f.write(chunk)
+
+                    files_downloaded.append(
+                        self.extract_file_dict(trimmed_source,
+                                               obj_data.headers))
+                    self.storage_dict['message'] = 'Source downloaded'
+                except FileNotFoundError as e:
+                    self.storage_dict['message'] = 'Destination not found'
 
         self.storage_dict['objlist'] = files_downloaded
         dictObj = self.update_dict(self.storage_dict['objlist'])
@@ -320,4 +370,35 @@ class Provider(StorageABC):
         self.storage_dict['filename'] = filename
         self.storage_dict['recursive'] = recursive
 
-        # TODO
+        if directory is None:
+            file_path = filename
+        else:
+            file_path = self.get_os_path(directory) / filename
+
+        if recursive is False:
+            objs = self.object_storage.list_objects(
+                self.namespace, self.bucket_name, prefix=str(file_path))
+        elif directory is None:
+            objs = self.object_storage.list_objects(
+                self.namespace, self.bucket_name)
+        else:
+            objs = self.object_storage.list_objects(
+                self.namespace, self.bucket_name, prefix=str(directory))
+
+        info_list = []
+
+        for obj in objs.data.objects:
+            if os.path.basename(obj.name) == filename:
+                info_list.append(self.get_and_extract_file_dict(obj.name))
+
+        self.storage_dict['objlist'] = info_list
+
+        if len(info_list) == 0:
+            self.storage_dict['message'] = 'File not found'
+        else:
+            self.storage_dict['message'] = 'File found'
+
+        pprint(self.storage_dict)
+        dictObj = self.update_dict(self.storage_dict['objlist'])
+        return dictObj
+
